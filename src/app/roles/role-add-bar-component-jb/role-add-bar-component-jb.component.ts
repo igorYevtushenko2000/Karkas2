c62142298e5074017f879f497b3497baf681fa58
igorYevtushenko2000 <i11am22from2000ukraine@gmail.com>
2020-05-22 00:26:20 +0300

    [M] change *A.html groups logic; add basic sccs to *A and *D grps

--- src/app/roles/role-add-bar-component-jb/role-add-bar-component-jb.component.ts
+++ src/app/roles/role-add-bar-component-jb/role-add-bar-component-jb.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-role-add-bar-component-jb',
   templateUrl: './role-add-bar-component-jb.component.html',
   styleUrls: ['./role-add-bar-component-jb.component.scss']
 })
